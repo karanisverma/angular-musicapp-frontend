a8fdbb40e3631da093e612b2744d55aea5e27212
suprabhasupi <suprabhasupi@gmail.com>
2016-07-06 01:29:07 +0530

    adding star rating

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
 (function() {
     var app = angular.module('music-app', ['ngMaterial', 'ngResource', 'ngRoute']);
     
+    app.directive('starRating', starRating);
+
+      function starRating() {
+    return {
+      restrict: 'EA',
+      template:
+        '<ul class="star-rating" ng-class="{readonly: readonly}">' +
+        '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)">' +
+        '    <i class="fa fa-star"></i>' + // or &#9733
+        '  </li>' +
+        '</ul>',
+      scope: {
+        ratingValue: '=ngModel',
+        max: '=?', // optional (default is 5)
+        onRatingSelect: '&?',
+        readonly: '=?'
+      },
+      link: function(scope, element, attributes) {
+        if (scope.max == undefined) {
+          scope.max = 5;
+        }
+        function updateStars() {
+          scope.stars = [];
+          for (var i = 0; i < scope.max; i++) {
+            scope.stars.push({
+              filled: i < scope.ratingValue
+            });
+          }
+        };
+        scope.toggle = function(index) {
+          if (scope.readonly == undefined || scope.readonly === false){
+            scope.ratingValue = index + 1;
+            scope.onRatingSelect({
+              rating: index + 1
+            });
+          }
+        };
+        scope.$watch('ratingValue', function(oldValue, newValue) {
+          if (newValue) {
+            updateStars();
+          }
+        });
+      }
+    };
+  }
 
     app.service('musicAppService', ['$resource', function($resource) {
         this.getResource = function(url) {
@@ -150,6 +195,7 @@
         '$mdMedia', 'musicAppService',
         function($scope, $resource, $mdDialog, $mdMedia, musicAppService) {
             var track = this;
+            track.rating1 = 2;
             var init_val = musicAppService.init_val;
             init_val.$promise.then(function(data) {
                 track.trackList = init_val.results;
